866acd7f481e0e9e1daa18378cfea268b4de3060
hyj <huyajuan@taiwu.com>
2018-06-22 14:22:03 +0800

    log

---
+++ AA_Cagetory/AA_Cagetory/CTMediator+AA_Cagetory.h
@@ -0,0 +1,13 @@
+//
+//  CTMediator+AA_Cagetory.h
+//  AA_Cagetory
+//
+//  Created by hyj on 2018/6/22.
+//  Copyright © 2018年 hyj. All rights reserved.
+//
+
+#import "CTMediator.h"
+
+@interface CTMediator (AA_Cagetory)
+
+@end
---
+++ AA_Cagetory/AA_Cagetory/CTMediator+AA_Cagetory.m
@@ -0,0 +1,13 @@
+//
+//  CTMediator+AA_Cagetory.m
+//  AA_Cagetory
+//
+//  Created by hyj on 2018/6/22.
+//  Copyright © 2018年 hyj. All rights reserved.
+//
+
+#import "CTMediator+AA_Cagetory.h"
+
+@implementation CTMediator (AA_Cagetory)
+
+@end
